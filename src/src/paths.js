9f80df90d865a1dce1570558457da40186259d20
Fedor <funtik21975@gmail.com>
2021-04-18 21:35:10 +0300

    Первый коммит.

---
+++ src/src/paths.js
@@ -0,0 +1,7 @@
+/**
+ * Пути к обработчикам медиа-дел.
+ */
+export const ckeditor_handle_path = {
+	medialib: '/bitrix/modules/prokl.ckeditor/files/medialib.php',
+	connector: '/bitrix/modules/prokl.ckeditor/files/connector.php',
+}
